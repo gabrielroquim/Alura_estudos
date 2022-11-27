4360e1ee8684384521299a1f3fddec8d930de5e5
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-27 12:03:47 -0300

    subindo testes

--- cypress_automacao/pratica/cypress.config.js
+++ cypress_automacao/pratica/cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '4nqb23',
   e2e: {
  reporter: "mochawesome",
  reporterOptions:{
   reporterDir: "cypress/report/mochawesome-report",
   overwrite: true,
   html: true,
-  json: true,
+  json: false,
   timestamp: "mmddyyyy_HHMMss"
  }
   },
