7f0c072675ab5f5f686b4404fdd9df6635df81d1
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-27 10:34:17 -0300

    teste usando cmd

--- cypress_automacao/pratica/cypress/e2e/3-AluraPic/aluraPic.cy.js
+++ cypress_automacao/pratica/cypress/e2e/3-AluraPic/aluraPic.cy.js
@@ -1,4 +1,7 @@
+/// <reference types="cypress" />
+
 describe('Login e registro de usuários alura pic', () => {
+
 
     beforeEach(() => {
         cy.visit('https://alura-fotos.herokuapp.com/')
@@ -6,6 +9,12 @@ describe('Login e registro de usuários alura pic', () => {
 
     it('Verifica mensagens validação', () => {
         cy.contains('a', 'Register now').click()
+        cy.contains('button', 'Register').click()
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible')
+        cy.contains('button', 'Register').click()
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible')
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible')
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible')
         
     });
 });
