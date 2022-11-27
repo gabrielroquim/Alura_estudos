c0cc307f5e849d0086e9ed42f5140467fbee0d8a
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-27 08:36:00 -0300

    1.8 aula

---
+++ cypress_automacao/pratica/cypress/e2e/3-AluraPic/aluraPic.cy.js
@@ -0,0 +1,9 @@
+describe('Login e registro de usuários alura pic', () => {
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com/')
+    });
+    it('Verifica mensagens validade', () => {
+        
+    });
+});
