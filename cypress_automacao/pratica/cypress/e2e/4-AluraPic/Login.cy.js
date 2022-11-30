baa2c20d09675e25602a0e4a0b26d5507acc2419
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-29 21:20:00 -0300

    testes each

--- cypress_automacao/pratica/cypress/e2e/4-AluraPic/Login.cy.js
+++ cypress_automacao/pratica/cypress/e2e/4-AluraPic/Login.cy.js
@@ -11,13 +11,12 @@ describe('Login e registro de usuários alura pic', () => {
     usuarios.forEach(usuario => {
 
         it(`Cadastrar usuário no alura pic ${usuario.userName}`, () => {
-            cy.contains('button', 'Register now').click()
-            cy.contains('button', 'Register').click()
+            cy.contains('a', 'Register now').click()            
             cy.get(':nth-child(1) > .form-control').type(usuario.email)
             cy.get(':nth-child(2) > .form-control').type(usuario.fullName)
-            cy.get(':nth-child(3) > .form-control').type(usuario.name)
+            cy.get(':nth-child(3) > .form-control').type(usuario.userName)
             cy.get(':nth-child(4) > .form-control').type(usuario.password)
-            cy.get('.btn').click({ force: true })
+            cy.contains('button', 'Register').click()           
             cy.get('.text-center').should('contain', 'Register to embrace a new world!')
         });
 
